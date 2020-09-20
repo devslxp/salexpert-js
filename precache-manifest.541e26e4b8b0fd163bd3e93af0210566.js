1c34efb9a61211877e60692ebf285e94174daee7
AM067745 <akshay.m@cerner.com>
2020-09-20 19:13:16 +0530

    Updates

---
+++ precache-manifest.541e26e4b8b0fd163bd3e93af0210566.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b0100bae8b171fa833a970d1969be6",
+    "url": "/salexpert.in/index.html"
+  },
+  {
+    "revision": "b6277fe4cc55e1ad5293",
+    "url": "/salexpert.in/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "804192c55adb99614c9a",
+    "url": "/salexpert.in/static/js/2.7de453f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/salexpert.in/static/js/2.7de453f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6277fe4cc55e1ad5293",
+    "url": "/salexpert.in/static/js/main.fd747e01.chunk.js"
+  },
+  {
+    "revision": "c93e5e9a27c8c49708f5",
+    "url": "/salexpert.in/static/js/runtime-main.09b5fa8a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/salexpert.in/static/media/logo.5d5d9eef.svg"
+  }
+]);
