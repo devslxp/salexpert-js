e8101726c3f7b5a06da5c2caee64c829f78347f5
AM067745 <akshay.m@cerner.com>
2020-09-21 17:22:52 +0530

    Remove images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import handshake from '../src/salexpert-webpage/common-components/images/handshake.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={handshake} className="logo-dimension" alt={logo}/>
+        {/* <img src={handshake} className="logo-dimension" alt={logo}/> */}
         <h3>
           SaleXpert.in
         </h3>
