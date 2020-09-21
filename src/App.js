7b047b21042b66b47acb5c01b52b4d5a5bb5865e
AM067745 <akshay.m@cerner.com>
2020-09-21 14:07:10 +0530

    Create under construction page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
+import handshake from '../src/salexpert-webpage/common-components/images/handshake.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={handshake} className="logo-dimension" alt={logo}/>
+        <h3>
+          SaleXpert.in
+        </h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Website is under construction.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Please visit again later.
+        </p>
       </header>
     </div>
   );
